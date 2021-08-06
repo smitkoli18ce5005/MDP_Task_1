7150c5a6643836f55a1231783a5e06a38a582e0e
Smit Koli <smit.koli@morningstar.com>
2021-08-07 00:23:49 +0530

    program6 added to p6

---
+++ program6.js
@@ -0,0 +1,8 @@
+//Create a function that will convert from Celsius to Fahrenheit
+
+let c = 40;
+let conv = 9/5;
+let f = (c * conv) + 32;
+
+console.log("In Celsius = " +c);
+console.log("In Fahrenheit = " +f);
